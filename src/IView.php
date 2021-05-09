738cc61c929d62aca427302320cb002833c895bb
Francis Carbah <fcarbah@gmail.com>
2021-05-09 11:36:44 -0400

    updated view interface

--- src/IView.php
+++ src/IView.php
@@ -6,21 +6,13 @@ namespace Feather\View;
  *
  * @author fcarbah
  */
-interface IView {
+interface IView
+{
+
     /**
-     * 
+     *
      * @param string $view
      * @param array $data
      */
-    public function render($view,array $data);
-    
-    /**
-     * 
-     * @param string $basePath
-     * @param string $cachePath
-     * @param array $options
-     * @return \Feather\View\IView
-     */
-    public static function getInstance($basePath,$cachePath,array $options=[]);
-    
+    public function render($view, array $data);
 }
